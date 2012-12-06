6e751d6409e94e3466d1bdfe633da73f58921bb9
David Björklund <david.bjorklund@gmail.com>
2012-12-06 10:44:45 +0100

    cleanup

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var assert = require('assert');
 
-var MinMax = module.exports = function(opts) {
+var minmax = module.exports = function(opts) {
     opts = opts || {};
 
     function minmax(val) {
@@ -22,35 +22,35 @@ var MinMax = module.exports = function(opts) {
     return minmax;
 }
 
-var minmax = MinMax();
-minmax(4711);
-assert.strictEqual(minmax.min, 4711);
-assert.strictEqual(minmax.max, 4711);
-assert.strictEqual(minmax.diff, 0);
+var mm = minmax();
+mm(4711);
+assert.strictEqual(mm.min, 4711);
+assert.strictEqual(mm.max, 4711);
+assert.strictEqual(mm.diff, 0);
 
-minmax = MinMax({ max: 0 });
-minmax(-4711);
-assert.strictEqual(minmax.max, 0);
-assert.strictEqual(minmax.min, -4711);
-assert.strictEqual(minmax.diff,4711);
+mm = minmax({ max: 0 });
+mm(-4711);
+assert.strictEqual(mm.max, 0);
+assert.strictEqual(mm.min, -4711);
+assert.strictEqual(mm.diff,4711);
 
-minmax = MinMax({ min: 0 });
-minmax(4711);
-assert.strictEqual(minmax.max, 4711);
-assert.strictEqual(minmax.min, 0);
-assert.strictEqual(minmax.diff, 4711);
+mm = minmax({ min: 0 });
+mm(4711);
+assert.strictEqual(mm.max, 4711);
+assert.strictEqual(mm.min, 0);
+assert.strictEqual(mm.diff, 4711);
 
-minmax = MinMax();
-minmax(new Date(0));
-minmax([new Date(-1000000), [new Date(1000000)]]);
-assert(minmax.max instanceof Date);
-assert(minmax.min instanceof Date);
-assert.strictEqual(minmax.min.getTime(), -1000000);
-assert.strictEqual(minmax.max.getTime(), 1000000);
-assert.strictEqual(minmax.diff, 2000000);
+mm = minmax();
+mm(new Date(0));
+mm([new Date(-1000000), [new Date(1000000)]]);
+assert(mm.max instanceof Date);
+assert(mm.min instanceof Date);
+assert.strictEqual(mm.min.getTime(), -1000000);
+assert.strictEqual(mm.max.getTime(), 1000000);
+assert.strictEqual(mm.diff, 2000000);
 
-minmax = MinMax();
-minmax([200, [-100]]);
-assert.strictEqual(minmax.max, 200);
-assert.strictEqual(minmax.min, -100);
-assert.strictEqual(minmax.diff, 300);
+mm = minmax();
+mm([200, [-100]]);
+assert.strictEqual(mm.max, 200);
+assert.strictEqual(mm.min, -100);
+assert.strictEqual(mm.diff, 300);
